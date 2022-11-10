f93bbb57c4634229c65c79b92dd29a8ae5955428
roxas <jeropastrana@gmail.com>
2022-11-10 16:06:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pmdm_simulacrocorregido_ejercicio1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pmdm_simulacrocorregido_ejercicio1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
